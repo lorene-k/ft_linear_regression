a288611f60bd8e6634c1a22d9b834175d8495a1d
Lorene <lorenejulie@hotmail.com>
2025-11-04 17:18:06 +0100

    small updates

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ else
         predict) python -m src.predict ;;
         precision) python -m src.bonus.precision ;;
         graph) python -m src.bonus.render_graph ;;
-        test) python -m tests.test ;;
+        test) pytest ;;
         *) echo "Invalid argument." ;;
     esac
 fi
